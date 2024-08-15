58afd6ab552d60a1a624dfcd87b5a226674fe1b8
sharath <29162020+tnfssc@users.noreply.github.com>
2024-08-15 17:40:01 +0530

    feat(logs): added debug logging

--- go.mod
+++ go.mod
@@ -6,6 +6,10 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/tmc/langchaingo v0.1.12 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,34 +3,37 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/atotto/clipboard"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
 )
 
 func main() {
-	args := os.Args
-	args = args[1:]
-
-	ctx := context.Background()
-	stmt := strings.Join(args, " ")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	debug := os.Getenv("DEBUG")
+	if debug == "true" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
 
 	apiKey := os.Getenv("GROQ_API_KEY")
 	if apiKey == "" {
-		log.Fatal("GROQ_API_KEY is not set")
+		log.Fatal().Msg("GROQ_API_KEY is not set")
 	}
 	shell := os.Getenv("SHELL")
 	if shell == "" {
-		log.Fatal("SHELL is not set")
+		log.Fatal().Msg("SHELL is not set")
 	}
 
 	prompt := fmt.Sprintf(`Only reply with the single line command surrounded by three backticks. It must be able to be directly run in the target shell. Do not include any other text.
 Make sure the command runs on %s shell.
-The prompt: %s`, shell, stmt)
+The prompt: %s`, shell, strings.Join(os.Args[1:], " "))
 
 	llm, err := openai.New(
 		openai.WithModel("gemma2-9b-it"),
@@ -38,19 +41,14 @@ The prompt: %s`, shell, stmt)
 		openai.WithToken(apiKey),
 	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to create llm")
 	}
-	completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+	completion, err := llms.GenerateFromSinglePrompt(context.Background(), llm, prompt)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to generate completion")
 	}
-	// remove the backticks
-	completion = strings.Trim(completion, "`")
-	completion = strings.Trim(completion, "`")
-	completion = strings.TrimSpace(completion)
-	fmt.Println(completion)
-
+	fmt.Println(strings.TrimSpace(strings.Trim(completion, "`")))
 	if err := clipboard.WriteAll(string(completion)); err != nil {
-		fmt.Println("Failed to copy to clipboard")
+		log.Debug().Err(err).Msg("Failed to copy to clipboard")
 	}
 }
