9722765b64e81cc3376ed2ac831c6e2ea6d83b21
sharath <29162020+tnfssc@users.noreply.github.com>
2025-03-02 21:24:45 +0000

    chore(deps): remove zerolog

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 
 	"github.com/atotto/clipboard"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
 )
@@ -18,30 +16,25 @@ var version string
 
 func main() {
 	if len(os.Args) == 1 {
+		fmt.Println("Missing prompt")
 		fmt.Println("Usage:\tgai [PROMPT]")
-		return
+		os.Exit(1)
 	}
 
 	if os.Args[1] == "version" {
 		fmt.Println(version)
-		return
-	}
-
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	debug := os.Getenv("DEBUG")
-	if debug == "true" {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else {
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		os.Exit(0)
 	}
 
 	apiKey := os.Getenv("GROQ_API_KEY")
 	if apiKey == "" {
-		log.Fatal().Msg("GROQ_API_KEY is not set. Get one from https://console.groq.com/keys.")
+		fmt.Println("GROQ_API_KEY is not set. Get one from https://console.groq.com/keys")
+		os.Exit(1)
 	}
 	shell := os.Getenv("SHELL")
 	if shell == "" {
-		log.Fatal().Msg("Could not determine shell. Set SHELL environment variable.")
+		fmt.Println("Could not determine shell. Set SHELL environment variable.")
+		os.Exit(1)
 	}
 	_shell := strings.Split(shell, "/")
 	shell = _shell[len(_shell)-1]
@@ -51,23 +44,25 @@ func main() {
 	prompt := fmt.Sprintf(`Only reply with the single line command surrounded by three backticks. It must be able to be directly run in the target shell. Do not include any other text.
 Make sure the command runs on %s shell on %s kernel.
 The prompt: %s`, shell, kernel, strings.Join(os.Args[1:], " "))
-	log.Debug().Msg(prompt)
 
-	llm, err := openai.New(
+	llm, _ := openai.New(
 		openai.WithModel("llama-3.3-70b-specdec"),
 		openai.WithBaseURL("https://api.groq.com/openai/v1"),
 		openai.WithToken(apiKey),
 	)
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to create llm")
-	}
+
 	completion, err := llms.GenerateFromSinglePrompt(context.Background(), llm, prompt)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to generate completion")
+		fmt.Println("Failed to generate command")
+		os.Exit(1)
 	}
-	completion = fmt.Sprintln(strings.TrimSpace(strings.Trim(completion, "`")))
-	fmt.Println(completion)
-	if err := clipboard.WriteAll(completion); err != nil {
-		log.Debug().Err(err).Msg("Failed to copy to clipboard")
+
+	completion = fmt.Sprint(strings.TrimSpace(strings.Trim(completion, "`")))
+	fmt.Println()
+	fmt.Println("", completion)
+
+	if err := clipboard.WriteAll(completion); err == nil {
+		fmt.Println()
+		fmt.Println(string("\033[32m"), "Copied \uf05d")
 	}
 }
